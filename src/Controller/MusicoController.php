0ba68008b43968bbd5e01468bae783674cf68c95
Cristian Toledo Valera <244533272+ctovnet@users.noreply.github.com>
2026-04-17 17:29:15 +0200

    Restauración tras creación de entidades para generar CRUD normal. Creado CRUD de Musico

--- src/Controller/MusicoController.php
+++ src/Controller/MusicoController.php
@@ -2,17 +2,80 @@
 
 namespace App\Controller;
 
+use App\Entity\Musico;
+use App\Form\MusicoType;
+use App\Repository\MusicoRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/musico')]
 final class MusicoController extends AbstractController
 {
-    #[Route('/musico', name: 'app_musico')]
-    public function index(): Response
+    #[Route(name: 'app_musico_index', methods: ['GET'])]
+    public function index(MusicoRepository $musicoRepository): Response
     {
         return $this->render('musico/index.html.twig', [
-            'controller_name' => 'MusicoController',
+            'musicos' => $musicoRepository->findAll(),
         ]);
+    }
+
+    #[Route('/new', name: 'app_musico_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $musico = new Musico();
+        $form = $this->createForm(MusicoType::class, $musico);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($musico);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_musico_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('musico/new.html.twig', [
+            'musico' => $musico,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_musico_show', methods: ['GET'])]
+    public function show(Musico $musico): Response
+    {
+        return $this->render('musico/show.html.twig', [
+            'musico' => $musico,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_musico_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Musico $musico, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(MusicoType::class, $musico);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_musico_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('musico/edit.html.twig', [
+            'musico' => $musico,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_musico_delete', methods: ['POST'])]
+    public function delete(Request $request, Musico $musico, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$musico->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($musico);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_musico_index', [], Response::HTTP_SEE_OTHER);
     }
 }
