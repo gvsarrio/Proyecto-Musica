fcc775687ee30197a68ecfd4f7578ecc80b93b50
gvsarrio <gabrielvicentesarrio@gmail.com>
2026-06-03 13:58:19 +0200

    fix despliegue 11

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# Asegurar que solo mpm_prefork está cargado
+rm -f /etc/apache2/mods-enabled/mpm_*.load /etc/apache2/mods-enabled/mpm_*.conf
+ln -sf /etc/apache2/mods-available/mpm_prefork.load /etc/apache2/mods-enabled/mpm_prefork.load
+ln -sf /etc/apache2/mods-available/mpm_prefork.conf /etc/apache2/mods-enabled/mpm_prefork.conf
+
 php bin/console doctrine:migrations:migrate --no-interaction --env=prod
 
 exec "$@"
