a6d22863c3969791afd625563bb0f424b5e3e47e
gvsarrio <gabrielvicentesarrio@gmail.com>
2026-06-12 15:20:21 +0200

    fix scroll chat

--- filter_coauthor.ps1
+++
@@ -1,7 +0,0 @@
-﻿foreach ($branch in (git branch -r --format='%(refname)' -- | Where-Object {$_ -match 'refs/heads/'})) {
-    $branchName = $branch -replace 'refs/heads/', ''
-    git checkout $branchName
-    foreach ($commit in (git log --format='%H' --grep="Co-Authored-By: Claude" -- )) {
-        git rebase -i "$($commit)^" -x "git commit --amend -m '$(git log -1 --format=%B $commit | Select-String -NotMatch 'Co-Authored' -Raw)'"
-    }
-}
